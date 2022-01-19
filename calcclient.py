3d493ea2bd94a4e79d7b730fed0ba7ffab88daed
codx1 <gabr.fern99@gmail.com>
2022-01-19 10:14:59 -0300

    Add loop mode

--- calcclient.py
+++ calcclient.py
@@ -7,21 +7,25 @@ PORT = 15000              # The same port as used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    mop = input("""MATH OPERATIONS
+    while True:
+        mop = input("""MATH OPERATIONS
     1 - SUM
     2 - SUB
     3 - DIV
     4 - MULT
+    5 - Exit program
     Input: """)
-    if mop == "1":
-        usinp = input("Input X and Y: ")
-    if mop == "2":
-        usinp = input("Input X and Y: ")
-    if mop == "3":
-        usinp = input("Input X and Y: ")
-    if mop == "4":
-        usinp = input("Input X and Y: ")
-    data_to_send = f"{mop} {usinp}".encode()
-    s.sendall(data_to_send)
-    data = s.recv(1024)
-print('Math result received by server: ', data.decode())
+        if mop == "1":
+            usinp = input("Input X and Y: ")
+        if mop == "2":
+            usinp = input("Input X and Y: ")
+        if mop == "3":
+            usinp = input("Input X and Y: ")
+        if mop == "4":
+            usinp = input("Input X and Y: ")
+        if mop == "5":
+            break
+        data_to_send = f"{mop} {usinp}".encode()
+        s.sendall(data_to_send)
+        data = s.recv(1024)
+        print('Math result received by server: ', data.decode())
