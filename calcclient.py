874c1c250c52abbd6a2801bfb1a625eebc01f04c
codx1 <gabr.fern99@gmail.com>
2022-01-19 10:04:28 -0300

    Add client

---
+++ calcclient.py
@@ -0,0 +1,27 @@
+# Echo client program
+import socket
+
+
+HOST = '192.168.23.11'    # The remote host
+PORT = 15000              # The same port as used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    mop = input("""MATH OPERATIONS
+    1 - SUM
+    2 - SUB
+    3 - DIV
+    4 - MULT
+    Input: """)
+    if mop == "1":
+        usinp = input("Input X and Y: ")
+    if mop == "2":
+        usinp = input("Input X and Y: ")
+    if mop == "3":
+        usinp = input("Input X and Y: ")
+    if mop == "4":
+        usinp = input("Input X and Y: ")
+    data_to_send = f"{mop} {usinp}".encode()
+    s.sendall(data_to_send)
+    data = s.recv(1024)
+print('Math result received by server: ', data.decode())
